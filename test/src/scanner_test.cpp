28c082dff8002102060e4f0bddba1a68288da9c5
Malibongwe <malib2027@gmail.com>
2024-05-02 19:33:50 +0200

    cleanup scanner_test

--- test/src/scanner_test.cpp
+++ test/src/scanner_test.cpp
@@ -13,7 +13,6 @@ class ScannerTest : public testing::Test
 {
 protected:
   Scanner sc1{ ":;" };
-  // Scanner sc2{ ";" };
 };
 
 TEST_F(ScannerTest, TestSingleToken)
@@ -28,20 +27,6 @@ TEST_F(ScannerTest, TestSingleToken)
   bool are_eq = std::equal(sc1_tks.begin(), sc1_tks.end(), exp_sc1_tks.begin(), compare);
   EXPECT_TRUE(are_eq);
 }
-
-/*
-TEST_F(ScannerTest, TestSemiColonToken)
-{
-  std::vector<Token> exp_sc2_tks{ Token{ TokenType::t_semicolon, 1 } };
-  std::vector<Token> sc2_tks{ sc2.scan_tokens() };
-
-  ASSERT_EQ(sc2_tks.size(), exp_sc2_tks.size());
-
-  auto compare = [](auto const &tk1, auto const &tk2) { return tk1.type == tk2.type && tk1.position == tk2.position; };
-
-  bool are_eq = std::equal(sc2_tks.begin(), sc2_tks.end(), exp_sc2_tks.begin(), compare);
-  EXPECT_TRUE(are_eq);
-} */
 
 }// namespace blang
 
